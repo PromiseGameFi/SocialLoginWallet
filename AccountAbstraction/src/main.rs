d2c28e70c4eef31da32ecf20ff47c7ef8dae3aaf
Promise Emmanuel <95595405+PromiseGameFi@users.noreply.github.com>
2024-10-22 12:06:55 +0100

    Update main.rs

--- AccountAbstraction/src/main.rs
+++ AccountAbstraction/src/main.rs
@@ -28,18 +28,7 @@ impl AbstractAccount {
 
 
 
-    fn verify_transaction(&self, transaction: &Transaction, signatures: &[SignatureShare]) -> bool {
-        if signatures.len() < self.threshold {
-            return false;
-        }
-
-        let message = self.hash_transaction(transaction);
-        let combined_signature = self.public_key_set
-            .combine_signatures(signatures)
-            .expect("Failed to combine signatures");
-
-        self.public_key_set.public_key().verify(&combined_signature, message)
-    }
+    
 
     fn hash_transaction(&self, transaction: &Transaction) -> H256 {
         let mut hasher = Keccak256::new();
