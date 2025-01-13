fd82f9b2d090257e289e2629af62f32442047905
Promise Emmanuel <95595405+PromiseGameFi@users.noreply.github.com>
2025-01-13 17:51:30 +0100

    Update shamir.rs

--- SSSharing/src/shamir.rs
+++ SSSharing/src/shamir.rs
@@ -157,7 +157,11 @@ impl<T, const N: usize> IntoIterator for FieldArray<T, N> {
     }
 }
 
-
+impl<T, const N: usize> From<[T; N]> for FieldArray<T, N> {
+    fn from(value: [T; N]) -> Self {
+        Self(value)
+    }
+}
 
 #[cfg(test)]
 impl<T: Random, const N: usize> Random for FieldArray<T, N> {
