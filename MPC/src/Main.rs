f8c68471703e51ad97b87f80b1c14d21934f3317
Promise Emmanuel <95595405+PromiseGameFi@users.noreply.github.com>
2025-01-20 20:42:51 +0100

    Update Main.rs

--- MPC/src/Main.rs
+++ MPC/src/Main.rs
@@ -7,19 +7,20 @@ mod utils;
 
 // SPDX-License-Identifier: Apache-2.0
 
-use std::{array::TryFromSliceError, fmt::Debug, fs::read_to_string, path::Path};
 
-use eyre::{ensure, eyre, Result};
-use fastcrypto::hash::{HashFunction, Sha256};
-use gf256::gf256;
-use rand::{CryptoRng, RngCore};
 
 use crate::{
     shamir::{FieldArray, ShamirSecretSharing, ShamirShare},
     utils::{bits_to_bytes, bytes_to_bits},
 };
 
-
+/// Parameters of the bip-39 specification (24 words variant).
+const DICTIONARY_INDICES_BITS: usize = 11;
+const MNEMONIC_WORDS: usize = 24;
+const DICTIONARY_WORDS: usize = 2 << (DICTIONARY_INDICES_BITS - 1);
+const CHECKSUM_BITS: usize = (MNEMONIC_WORDS * DICTIONARY_INDICES_BITS) / 33;
+const ENTROPY_BITS: usize = CHECKSUM_BITS * 32;
+const ENTROPY_BYTES: usize = ENTROPY_BITS / 8;
 
 pub struct Bip39Dictionary {
     words: [String; DICTIONARY_WORDS],
