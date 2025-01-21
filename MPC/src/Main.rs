8087ffc43fe777dfdcc9659d95d11c22ecbba02d
Promise Emmanuel <95595405+PromiseGameFi@users.noreply.github.com>
2025-01-21 21:22:31 +0100

    Update Main.rs

--- MPC/src/Main.rs
+++ MPC/src/Main.rs
@@ -44,7 +44,73 @@ pub struct Bip39Dictionary {
     words: [String; DICTIONARY_WORDS],
 }
 
+impl Entropy {
+    pub fn as_bits(&self) -> &[bool] {
+        &self.0
+    }
 
+    pub fn to_bytes(&self) -> [u8; ENTROPY_BYTES] {
+        bits_to_bytes(&self.0).try_into().unwrap()
+    }
+
+    #[cfg(test)]
+    pub fn random<R: CryptoRng + RngCore>(rng: &mut R) -> Self {
+        use rand::Rng;
+
+        Self(std::array::from_fn(|_| rng.gen()))
+    }
+}
+
+impl TryFrom<&[bool]> for Entropy {
+    type Error = TryFromSliceError;
+
+    fn try_from(value: &[bool]) -> Result<Self, Self::Error> {
+        Ok(Self(value.try_into()?))
+    }
+}
+
+impl<T> From<FieldArray<T, ENTROPY_BYTES>> for Entropy
+where
+    u8: From<T>,
+{
+    fn from(value: FieldArray<T, ENTROPY_BYTES>) -> Self {
+        let bytes = value.into_iter().map(u8::from).collect::<Vec<_>>();
+        bytes_to_bits(&bytes).as_slice().try_into().unwrap()
+    }
+}
+
+impl<T> From<&Entropy> for FieldArray<T, ENTROPY_BYTES>
+where
+    T: From<u8> + Debug,
+{
+    fn from(value: &Entropy) -> Self {
+        value.to_bytes().map(T::from).into()
+    }
+}
+
+/// The checksum of a bip-39 secret.
+#[derive(PartialEq, Eq)]
+#[cfg_attr(test, derive(Clone, Debug))]
+struct Checksum([bool; CHECKSUM_BITS]);
+
+impl TryFrom<&[bool]> for Checksum {
+    type Error = TryFromSliceError;
+
+    fn try_from(value: &[bool]) -> Result<Self, Self::Error> {
+        Ok(Self(value.try_into()?))
+    }
+}
+
+impl From<&Entropy> for Checksum {
+    fn from(entropy: &Entropy) -> Self {
+        let digest = Sha256::digest(entropy.to_bytes());
+        let bits = bytes_to_bits(digest.as_ref());
+        let checksum = bits[..CHECKSUM_BITS]
+            .try_into()
+            .expect("Slice size should match the checksum bit length");
+        Self(checksum)
+    }
+}
 
 
 
